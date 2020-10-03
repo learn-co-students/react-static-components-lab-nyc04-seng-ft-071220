b7cfa81d3aecda323c2b2d6b87fe25cc840837e2
Dfelix02 <david.felix02@hotmail.com>
2020-10-03 06:50:29 -0400

    Done.

---
+++ src/catJamming.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CatJamming extends Component {
+    state = {  }
+    render() { 
+        return (
+        <div className="bar" id="cat-jamming">
+            <img src="/ayeCatJamming.gif" alt="catjamming to music" />
+        </div> );
+    }
+}
+
+export default CatJamming;
